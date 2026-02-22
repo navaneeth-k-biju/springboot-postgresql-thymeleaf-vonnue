77a518e5bdc71004231c452e1fc5a803fefccf6b
navaneeth@2003 <navaneethkbiju2003@gmail.com>
2026-02-22 10:30:00 +0530

    base server

---
+++ src/main/java/com/decisions/SpringbootMysqlThymeleafVonnueApplication.java
@@ -0,0 +1,13 @@
+package com.decisions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlThymeleafVonnueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlThymeleafVonnueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decisions/SpringbootMysqlThymeleafVonnueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decisions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlThymeleafVonnueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
